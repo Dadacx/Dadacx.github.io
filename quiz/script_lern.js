b84b0f89817494c0879e5c0d54f7c41d7db7576a
Dadacx <84072588+Dadacx@users.noreply.github.com>
2023-10-10 22:15:48 +0200

    Update script_lern.js

--- quiz/script_lern.js
+++ quiz/script_lern.js
@@ -3,6 +3,24 @@ const odp = []
 rng = 0
 const test = []
 ilosc = 0
+function search() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 function sortTable() {
     var table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById("table");
