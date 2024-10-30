3d4c00d49af98f747aeb96f317d023604d22caa2
Dadacx <84072588+Dadacx@users.noreply.github.com>
2024-10-30 22:26:30 +0100

    Update script.js

--- quiz2/script.js
+++ quiz2/script.js
@@ -1,45 +1,4 @@
-const pytanie = []
-const odp = []
-rng = 0
-const test = []
-ilosc = 1
-zle = 0
-  function next() {
-      document.getElementById("pytanie").innerHTML = pytanie[rng]
-      document.getElementById("in").value = null
-      document.getElementById("odp_btn").style.visibility = "hidden"
-      document.getElementById("blad").style.visibility = "hidden"
-      document.getElementById("odp").innerHTML = "&nbsp"
-  }
- function repeatingNumber() {
-     yes = 0
-     for(let i = 0; i < test.length +1; i++) {
-         if(rng==test[i]) {
-            getRandomNumber(0,pytanie.length) //tu zmienić <-----------------------------------------------
-            yes = 0
-            break;
-         } else yes = 1
-     }
-     if(yes==1) {
-         next()
-     }
-     test.push(rng)
-     console.log(test)
- }
- function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    rng = Math.floor(Math.random() * (max - min)) + min;
-    repeatingNumber()
-  }
-function load() {
-    fetch("https://script.google.com/macros/s/AKfycbxWaGf9FNpQaxxb2Od4_lo3BFDUGHsGWF4rtpaz9C75NvckuZAe3Q6KRfOXtlZbtFvD/exec")
-  .then(res => res.json()).then(data => {for (let i = 1; i < data.content.length; i++) {
-    //console.log(data.content[i]);
-    pytanie[i-1] = data.content[i][0]
-    odp[i-1] = data.content[i][1]
-  }
-  pytanie = [
+const pytanie = [
     "pierścionek",
     "pójść wcześniej spać",
     "rozstać się",
@@ -274,7 +233,7 @@ function load() {
     "uporządkować coś",
     "przypaść sobie do serca"
 ]
-  odp = [
+  const odp = [
     "ring",
     "get an early night",
     "split up",
@@ -509,6 +468,45 @@ function load() {
     "do sth out",
     "get on like a house on fire"
 ]
+rng = 0
+const test = []
+ilosc = 1
+zle = 0
+  function next() {
+      document.getElementById("pytanie").innerHTML = pytanie[rng]
+      document.getElementById("in").value = null
+      document.getElementById("odp_btn").style.visibility = "hidden"
+      document.getElementById("blad").style.visibility = "hidden"
+      document.getElementById("odp").innerHTML = "&nbsp"
+  }
+ function repeatingNumber() {
+     yes = 0
+     for(let i = 0; i < test.length +1; i++) {
+         if(rng==test[i]) {
+            getRandomNumber(0,pytanie.length) //tu zmienić <-----------------------------------------------
+            yes = 0
+            break;
+         } else yes = 1
+     }
+     if(yes==1) {
+         next()
+     }
+     test.push(rng)
+     console.log(test)
+ }
+ function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    rng = Math.floor(Math.random() * (max - min)) + min;
+    repeatingNumber()
+  }
+function load() {
+    fetch("https://script.google.com/macros/s/AKfycbxWaGf9FNpQaxxb2Od4_lo3BFDUGHsGWF4rtpaz9C75NvckuZAe3Q6KRfOXtlZbtFvD/exec")
+  .then(res => res.json()).then(data => {for (let i = 1; i < data.content.length; i++) {
+    //console.log(data.content[i]);
+    //pytanie[i-1] = data.content[i][0]
+    //odp[i-1] = data.content[i][1]
+  }
   getRandomNumber(0,pytanie.length) //tu zmienić <------------------------------------------------------
   document.getElementById("pytanie").innerHTML = pytanie[rng]
   document.getElementById("progress").innerHTML = "Pytanie: " + parseInt(ilosc) + "/" + pytanie.length})
